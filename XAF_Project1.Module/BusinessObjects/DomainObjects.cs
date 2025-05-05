614ec130b91adf66a052bb76bff0bab3b6bc48a8
Sajad Shavei <sajadshavei18@gmail.com>
2025-05-05 17:42:41 +0330

    Add project files.

---
+++ XAF_Project1.Module/BusinessObjects/DomainObjects.cs
@@ -0,0 +1,65 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+using System;
+
+namespace XAF_Project1.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    [NavigationItem("Planning")]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class DomainObjects : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+       
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+        public DomainObjects(Session session) : base(session) { }
+        [Size(255)]
+        public string Subject { get; set; }
+        public ProjectTaskStatus Status { get; set; }
+        public Person AssignedTo { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        [Size(SizeAttribute.Unlimited)]
+        public string Notes { get; set; }
+        [Association]
+        public Project Project { get; set; }
+    }
+    [NavigationItem("Planning")]
+    public class Project : BaseObject
+    {
+        public Project(Session session) : base(session) { }
+        public string Name { get; set; }
+        public Person Manager { get; set; }
+        [Size(SizeAttribute.Unlimited)]
+        public string Description { get; set; }
+        [Association, DevExpress.ExpressApp.DC.Aggregated]
+        public XPCollection<DomainObjects> Tasks
+        {
+            get { return GetCollection<DomainObjects>("Tasks"); }
+        }
+    }
+    public enum ProjectTaskStatus
+    {
+        NotStarted = 0,
+        InProgress = 1,
+        Completed = 2,
+        Deferred = 3
+    }
+}
---
+++ XAF_Project1.Module/Controllers/ProjectTaskController.cs
@@ -0,0 +1,65 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.ConditionalAppearance;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using XAF_Project1.Module.BusinessObjects;
+
+namespace XAF_Project1.Module.Controllers
+{
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    [Appearance("Completed1", TargetItems = "Subject",
+    Criteria = "Status = 'Completed'", FontStyle = FontStyle.Strikeout, FontColor = "ForestGreen")]
+    [Appearance("Completed2", TargetItems = "*;Status;AssignedTo",
+    Criteria = "Status = 'Completed'", Enabled = false)]
+    [Appearance("InProgress", TargetItems = "Subject;AssignedTo",
+    Criteria = "Status = 'InProgress'", BackColor = "LemonChiffon")]
+    [Appearance("Deferred", TargetItems = "Subject",
+    Criteria = "Status = 'Deferred'", BackColor = "MistyRose")]
+    [RuleCriteria("EndDate >= StartDate")]
+    public class ProjectTaskController : ViewController
+    {
+        public ProjectTaskController()
+        {
+            TargetObjectType = typeof(DomainObjects);
+            TargetViewType = ViewType.Any;
+            SimpleAction markCompletedAction = new SimpleAction(
+                this, "MarkCompleted",
+                DevExpress.Persistent.Base.PredefinedCategory.RecordEdit)
+            {
+                TargetObjectsCriteria =
+                    (CriteriaOperator.Parse("Status != ?", ProjectTaskStatus.Completed)).ToString(),
+                ConfirmationMessage =
+                            "Are you sure you want to mark the selected task(s) as 'Completed'?",
+                ImageName = "State_Task_Completed"
+            };
+            markCompletedAction.Execute += (s, e) =>
+            {
+                foreach (DomainObjects task in e.SelectedObjects)
+                {
+                    task.EndDate = DateTime.Now;
+                    task.Status = ProjectTaskStatus.Completed;
+                    View.ObjectSpace.SetModified(task);
+                }
+                View.ObjectSpace.CommitChanges();
+                View.ObjectSpace.Refresh();
+            };
+        }
+    }
+}
